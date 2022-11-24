060a0f6107d60d9217c7fb3ceec58755d04cdc0e
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2022-11-24 16:46:19 -0500

    commitr

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,8 @@ import { NavBar } from "./components";
 export default function App(){
     
     return (
-        <>
-            <NavBar/>
-            <text>
-                im a test change
-            </text>
-        </>
+        
+        <NavBar/>
+  
     );
 }
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,11 +1,38 @@
-
+import { useEffect } from 'react';
+import {
+    Navbar,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarText,
+} from 'reactstrap';
 
 
 export default function NavBar(){
-    
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <div>
-
+            <Navbar
+                className="my-2"
+                color="dark"
+                dark
+            >
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink href="/components/">Components</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">
+                            GitHub
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <NavbarText>Simple Text</NavbarText>
+            </Navbar>
         </div>
     );
 }
