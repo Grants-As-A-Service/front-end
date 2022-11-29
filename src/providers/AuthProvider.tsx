9593b3a5f163cc7ab0c60ff650e3b53aabf8d29a
Magnus Reeves <71095829+rreeves8@users.noreply.github.com>
2022-11-28 20:54:07 -0500

    upload

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,23 +1,19 @@
-import { createContext, useMemo, useState } from "react"
+import { createContext, useMemo, useState } from "react";
 
 type AuthContextADT = {
-    isLoggedIn: boolean,
-    setLoggedIn: (loggedIn: boolean) => void
-}
+    isLoggedIn: boolean;
+    setLoggedIn: (loggedIn: boolean) => void;
+};
 
-export const AuthContext = createContext<AuthContextADT>({} as AuthContextADT)
+export const AuthContext = createContext<AuthContextADT>({} as AuthContextADT);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-    const [isLoggedIn, setLoggedIn] = useState<boolean>(false)
+    const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
 
     //could use different pattern here for checking auth state when page loads
     useMemo(() => {
         //check here for logged in and set it
-    },[])
+    }, []);
 
-    return (
-        <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    return <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>{children}</AuthContext.Provider>;
+};
