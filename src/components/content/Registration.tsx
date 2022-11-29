8a17e30b71fab34ef89b6da948c5a2ab0c2b044f
jaysondale <jaysondale1@gmail.com>
2022-11-29 18:28:05 -0500

    merged registration form files

--- src/components/content/Registration.tsx
+++ src/components/content/Registration.tsx
@@ -1,8 +1,12 @@
-import { Form, FormGroup, Label, Input, FormText, Button } from "reactstrap";
+import { Form, FormGroup, Label, Input, FormText, Button, Row, Col } from "reactstrap";
 import { SwapScreenADT } from "../content-stack/NavigationStack";
 import Icon from "../items/icon";
+import FormData from "../../static/FormData.json";
+
 
 export default function Registration({ swapScreen }: { swapScreen: SwapScreenADT }) {
+    const industryList = FormData.industries;
+    const provinces = FormData.provinces;
     return (
         <>
             <div style={{ position: "absolute" }}>
@@ -11,65 +15,132 @@ export default function Registration({ swapScreen }: { swapScreen: SwapScreenADT
                 </div>
             </div>
             <div style={{ padding: "5%" }}>
-                <Form>
-                    <p className="fs-1">Registration</p>
-                    <FormGroup>
-                        <Label for="exampleEmail">Email</Label>
-                        <Input id="exampleEmail" name="email" placeholder="with a placeholder" type="email" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="examplePassword">Password</Label>
-                        <Input id="examplePassword" name="password" placeholder="password placeholder" type="password" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleSelect">Select</Label>
-                        <Input id="exampleSelect" name="select" type="select">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleSelectMulti">Select Multiple</Label>
-                        <Input id="exampleSelectMulti" multiple name="selectMulti" type="select">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </Input>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleText">Text Area</Label>
-                        <Input id="exampleText" name="text" type="textarea" />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleFile">File</Label>
-                        <Input id="exampleFile" name="file" type="file" />
-                        <FormText>
-                            This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
-                        </FormText>
-                    </FormGroup>
-                    <FormGroup tag="fieldset">
-                        <legend>Radio Buttons</legend>
-                        <FormGroup check>
-                            <Input name="radio1" type="radio" /> <Label check>Option one is this and that—be sure to include why it‘s great</Label>
-                        </FormGroup>
-                        <FormGroup check>
-                            <Input name="radio1" type="radio" />{" "}
-                            <Label check>Option two can be something else and selecting it will deselect option one</Label>
-                        </FormGroup>
-                        <FormGroup check disabled>
-                            <Input disabled name="radio1" type="radio" /> <Label check>Option three is disabled</Label>
-                        </FormGroup>
-                    </FormGroup>
-                    <FormGroup check>
-                        <Input type="checkbox" /> <Label check>Check me out</Label>
-                    </FormGroup>
-                    <Button>Submit</Button>
-                </Form>
+                <Row className="justify-content-center">
+                    <div className="d-flex w-50 flex-column mt-5">
+                        <h3>Business User Registration</h3>
+                        <Form>
+                            <h5>Personal Info</h5>
+                            <FormGroup>
+                                <Input type="text" 
+                                    name="name"
+                                    id="name"
+                                    placeholder="Name"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" 
+                                    name="email"
+                                    id="email"
+                                    placeholder="Email"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" 
+                                    name="phone"
+                                    id="phone"
+                                    placeholder="Phone"
+                                />
+                            </FormGroup>
+                            <h5 className="mt-5">Business Info</h5>
+                            <FormGroup>
+                                <Input type="text" 
+                                    name="businessName"
+                                    id="businessName"
+                                    placeholder="Business Name"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" 
+                                    name="businessPhone"
+                                    id="businessPhone"
+                                    placeholder="Business Phone"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Row>
+                                    <Col md={8} xs={12}>
+                                        <Input type="text" 
+                                            name="address"
+                                            id="address"
+                                            placeholder="Address"
+                                        />
+                                    </Col>
+                                    <Col md={4} xs={12}>
+                                        <Input type="text" 
+                                            name="city"
+                                            id="city"
+                                            placeholder="City"
+                                        />
+                                    </Col>
+                                </Row>
+                                <Row className="mt-2">
+                                    <Col md={6} xs={12}>
+                                        <Input type="select" 
+                                            name="province"
+                                            id="province"
+                                            placeholder="Province"
+                                        >
+                                            {provinces.map(province => {
+                                                return <option>{province.name}</option>
+                                            })}    
+                                        </Input>
+                                    </Col>
+                                    <Col md={6} xs={12}>
+                                        <Input type="text" 
+                                            name="postal"
+                                            id="postal"
+                                            placeholder="Postal Code"
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="industry">Industry</Label>
+                                <Input type="select"
+                                    name="industry"
+                                    id="industry"
+                                >
+                                    {industryList.map(industry => {
+                                        return <option>{industry}</option>
+                                    })}
+                                </Input>
+                            </FormGroup>
+                            <FormGroup>
+                                <Row className="mt-2">
+                                    <Col md={6} xs={12}>
+                                        <Input type="number" 
+                                            name="fte"
+                                            id="fte"
+                                            placeholder="# Full Time Employees"
+                                        />
+                                    </Col>
+                                    <Col md={6} xs={12}>
+                                    <Input type="number" 
+                                            name="pte"
+                                            id="pte"
+                                            placeholder="# Part Time Employees"
+                                        />
+                                    </Col>
+                                </Row>
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="number"
+                                    name="yearOfInc"
+                                    id="yearOfInc"
+                                    placeholder="Year of Inception"
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="number"
+                                    name="revenue"
+                                    id="revenue"
+                                    placeholder="Approx. Annual Revenue ($)"
+                                />
+                            </FormGroup>
+                            <Button color="primary" type="submit">Register</Button>
+                        </Form>
+                    </div>
+                </Row>
             </div>
         </>
     );
